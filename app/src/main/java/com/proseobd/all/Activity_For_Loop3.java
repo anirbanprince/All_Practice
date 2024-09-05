b94f2df31376ef56bab126e69b992a303bc18754
anirbanprince <anirbanprince@yahoo.com>
2024-09-05 21:03:42 +0600

    loop part 3

--- app/src/main/java/com/proseobd/all/Activity_For_Loop3.java
+++ app/src/main/java/com/proseobd/all/Activity_For_Loop3.java
@@ -14,10 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Activity_For_Loop3 extends AppCompatActivity {
-    EditText edtSnum, edtLnum;
-    Button btnCal;
-    TextView tvd;
-    int fNum, lNum;
+    EditText edtSnum, edtLnum, edtSyear, edtLyear;
+    Button btnCal, btnCal2;
+    TextView tvd, tvd2;
+    int fNum, lNum, fYear, lYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +26,14 @@ public class Activity_For_Loop3 extends AppCompatActivity {
 
         edtSnum = findViewById(R.id.edtSnum);
         edtLnum = findViewById(R.id.edtLnum);
+        edtSyear = findViewById(R.id.edtSyear);
+        edtLyear = findViewById(R.id.edtLyear);
+
         btnCal = findViewById(R.id.btnCal);
+        btnCal2 = findViewById(R.id.btnCal2);
+
         tvd = findViewById(R.id.tvd);
+        tvd2 = findViewById(R.id.tvd2);
 
 
         btnCal.setOnClickListener(new View.OnClickListener() {
@@ -60,6 +66,49 @@ public class Activity_For_Loop3 extends AppCompatActivity {
                 } else {
                     Toast.makeText(Activity_For_Loop3.this, "2nd number must be higher than 1st", Toast.LENGTH_SHORT).show();
                 }
+
+
+
+
+            }
+        });
+
+
+
+
+        btnCal2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (edtSyear.length()>0){
+                    fYear=Integer.parseInt(edtSyear.getText().toString());
+                } else {
+                    edtSyear.setError("Please Enter Valaid Year");
+                }
+
+
+                if (edtLyear.length()>0){
+                    lYear=Integer.parseInt(edtLyear.getText().toString());
+                } else {
+                    edtLyear.setError("Please Enter Valaid Year");
+                }
+
+
+                if (lYear>fYear){
+                    for (int x=fYear; x<=lYear; x++){
+                        if (x%100==0 || x%4==0 && x%400!=0){
+                            tvd2.append(""+x);
+                        }else {
+                            Toast.makeText(Activity_For_Loop3.this, "2nd number must be higher than 1st", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+
+                }
+
+
+
 
 
 
