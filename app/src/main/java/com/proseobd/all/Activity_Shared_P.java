17a8861fab8543a7a5a656122336f70a1a9e5c30
Anirban Prince <anirbanprince@yahoo.com>
2024-09-09 22:55:08 +0600

    Last

---
+++ app/src/main/java/com/proseobd/all/Activity_Shared_P.java
@@ -0,0 +1,21 @@
+package com.proseobd.all;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity_Shared_P extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_p);
+
+
+
+    }
+}
