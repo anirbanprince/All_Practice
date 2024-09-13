d7ed077f435a9449c18f3e4fcf284c5c3d6c8dc8
Anirban Prince <anirbanprince@yahoo.com>
2024-09-13 16:33:23 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_shared_p.xml

--- app/src/main/java/com/proseobd/all/Activity_Layout_Inflater.java
+++ app/src/main/java/com/proseobd/all/Activity_Layout_Inflater.java
@@ -1,6 +1,8 @@
 package com.proseobd.all;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Activity_Layout_Inflater extends AppCompatActivity {
+
+    Button btnAdd, btnRemove;
+    LinearLayout mainLayout, layoutExt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
