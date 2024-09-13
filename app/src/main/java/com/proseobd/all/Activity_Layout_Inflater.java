a0d1906f6a65b07defaac36ef58f45210ba0c023
anirbanprince <anirbanprince@yahoo.com>
2024-09-13 19:44:24 +0600

    sp edit

--- app/src/main/java/com/proseobd/all/Activity_Layout_Inflater.java
+++ app/src/main/java/com/proseobd/all/Activity_Layout_Inflater.java
@@ -1,6 +1,9 @@
 package com.proseobd.all;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -13,12 +16,32 @@ import androidx.core.view.WindowInsetsCompat;
 public class Activity_Layout_Inflater extends AppCompatActivity {
 
     Button btnAdd, btnRemove;
-    LinearLayout mainLayout, layoutExt;
+    LinearLayout mainLayout, layOut2, layoutExt;
+    LayoutInflater layoutInflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_inflater);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnRemove = findViewById(R.id.btnRemove);
+
+        mainLayout = findViewById(R.id.mainLayout);
+        layOut2 = findViewById(R.id.layOut2);
+
+        layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                layoutInflater.inflate(R.layout.layout_inflator_ext, layOut2);
+
+            }
+        });
 
 
 
